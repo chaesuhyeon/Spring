7399ccaabef5d4ce699c8ea0915c2bd9ac6b7594
suhyeon <ddd8177@sweetk.co.kr>
2022-06-07 13:14:49 +0900

    싱글톤 진행중

--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -1,14 +1,21 @@
 package hello.core.singleton;
 
+//자바가 뜨면서 static영역에 있는 instance를 초기화 하고
 public class SingletonService {
     private static final SingletonService instance = new SingletonService();
     // 자기 자신을 내부에 private으로 하나 생성 . static으로 생성
+    // 자기 자신을 생성해서 instance 변수에 참조로 넣어줌
 
     public static SingletonService getInstance() {
         return instance;
     }
 
-    public static void main(String[] args) {
+    // private으로 막아줌
+    private SingletonService() {
 
+    }
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출 ");
     }
 }
