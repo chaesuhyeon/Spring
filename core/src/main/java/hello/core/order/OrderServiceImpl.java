9d20ce409fe331c897b1b2d78a3e9d595b60c302
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-08-10 11:42:37 +0900

    생성자 주입test

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -15,8 +15,10 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository ;
     private final DiscountPolicy discountPolicy ;
 
-    @Autowired
+    @Autowired // 스프링 Bean인데 생성자가 1개일경우  @Autowired 생략가능. 생략해도 알아서 의존관계 주입 해줌 (스프링 빈일경우에만 해당)
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("memberRepository = " + memberRepository); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
+        System.out.println("discountPolicy = " + discountPolicy); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
