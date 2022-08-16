88a29d1729716c5940cbfb7be94dfe0bb8cfcdb4
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-08-16 23:44:56 +0900

    Lombok

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,26 +6,30 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final이 붙은 것을 생성자로 만들어줌(이것을 사용함으로 인해서 밑에 생성자 주석처리)
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository ;
     private final DiscountPolicy discountPolicy ;
 
-    @Autowired // 스프링 Bean인데 생성자가 1개일경우  @Autowired 생략가능. 생략해도 알아서 의존관계 주입 해줌 (스프링 빈일경우에만 해당)
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
-        System.out.println("discountPolicy = " + discountPolicy); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // ------------------------------------ Lombok의 @RequiredArgsConstructor 사용으로 생성자 주석처리 -----------------------------
 
-//    private DiscountPolicy discountPolicy;
+//    @Autowired // 스프링 Bean인데 생성자가 1개일경우  @Autowired 생략가능. 생략해도 알아서 의존관계 주입 해줌 (스프링 빈일경우에만 해당)
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        System.out.println("memberRepository = " + memberRepository); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
+//        System.out.println("discountPolicy = " + discountPolicy); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // -------------------------------------------------------------------------------------------------------------------
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
