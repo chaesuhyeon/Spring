39a5777a7bd75dfa668fa28863c35f5d1d67c6a6
suhyeon <ddd8177@sweetk.co.kr>
2022-08-18 13:06:07 +0900

    조회 빈이 2개 이상일 때

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,10 +8,11 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final이 붙은 것을 생성자로 만들어줌(이것을 사용함으로 인해서 밑에 생성자 주석처리)
+//@RequiredArgsConstructor // final이 붙은 것을 생성자로 만들어줌(이것을 사용함으로 인해서 밑에 생성자 주석처리)
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository ;
@@ -21,12 +22,12 @@ public class OrderServiceImpl implements OrderService {
     // ------------------------------------ Lombok의 @RequiredArgsConstructor 사용으로 생성자 주석처리 -----------------------------
 
 //    @Autowired // 스프링 Bean인데 생성자가 1개일경우  @Autowired 생략가능. 생략해도 알아서 의존관계 주입 해줌 (스프링 빈일경우에만 해당)
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        System.out.println("memberRepository = " + memberRepository); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
-//        System.out.println("discountPolicy = " + discountPolicy); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("memberRepository = " + memberRepository); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
+        System.out.println("discountPolicy = " + discountPolicy); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     // -------------------------------------------------------------------------------------------------------------------
 
