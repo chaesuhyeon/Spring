397016d01d930d6b38c32220ba3668f3c4a266c5
chaesuhyeon <ddd8177@gmail.com>
2022-08-21 12:19:18 +0900

    어노테이션 직접 만들기 MainDiscountPolicy

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,8 +8,9 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-// @Qualifier("mainDiscountPolicy")
-@Primary
+// @Qualifier("mainDiscountPolicy") // 컴파일할때 문자체크를 해주지 않아서 오타가 나도 컴파일 오류가 발생하지 않음. --> 직접 만든 MainDiscountPolicy 어노테이션 사용
+//@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements  DiscountPolicy{
     private int discountPercent = 10;
 
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -22,7 +23,7 @@ public class OrderServiceImpl implements OrderService {
     // ------------------------------------ Lombok의 @RequiredArgsConstructor 사용으로 생성자 주석처리 -----------------------------
 
 //    @Autowired // 스프링 Bean인데 생성자가 1개일경우  @Autowired 생략가능. 생략해도 알아서 의존관계 주입 해줌 (스프링 빈일경우에만 해당)
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         System.out.println("memberRepository = " + memberRepository); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
         System.out.println("discountPolicy = " + discountPolicy); // @Autowired 주석 처리하고 AutoAppConfigTest 실행해봐서 값들어오는지 확인
         this.memberRepository = memberRepository;
