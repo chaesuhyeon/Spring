b470c99bd0897486ebe1ce18cffd21693573abf9
suhyeon chae <ddd8177@gmail.com>
2022-08-29 20:22:40 +0900

    웹 스코프 문제 Provider 사용해서 해결

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +13,19 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+//    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
 
     @RequestMapping("log-demo")
     @ResponseBody // 화면 없는 경우 문자로 바로 반환하고 싶을 때
-    public String logDemo(HttpServletRequest request){ // HttpServletRequest 사용하면 고객 요청 정보 받을 수 있음
+    public String logDemo(HttpServletRequest request) throws InterruptedException { // HttpServletRequest 사용하면 고객 요청 정보 받을 수 있음
+
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
-
+        Thread.sleep(1000);
         logDemoService.logic("testId");
 
         return "OK";
