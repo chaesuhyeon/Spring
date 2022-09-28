27c47ebcda119d8fa83dca2a9630c5aee5882789
suhyeon chae <ddd8177@gmail.com>
2022-09-28 21:20:34 +0900

    HTTP 응답

---
+++ spring-mvc/springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,59 @@
+package hello.springmvc.basic.response;
+
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+//@RestController // ResponseBody를 붙이지 않고 사용할때
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2(){
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @ResponseBody // ResponseBody를 사용하면 view를 사용하지 않고 HTTP 메시지 컨버터를 통해서 HTTP메시지를 직접 입력할 수 있음
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3(){
+        return "ok";
+    }
+
+    // json 처리
+    // ResponseEntity를 반환. HTTP메시지 컨버터를 통해서 JSON 형식으로 변환되어서 반환
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    // json 처리
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return helloData;
+    }
+
+}
