980063b7a1757885029c5169a6c8f76074f4321a
chaesuhyeon <ddd8177@gmail.com>
2022-10-01 21:11:42 +0900

    상품 상세 페이지 & 상품 등록 폼

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -31,6 +33,31 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    //item 상세보기
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId , Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    //상품 등록 폼 보여주기
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    //상품 등록
+    @PostMapping("/add")
+    public String save(){
+        return "basic/addForm";
+    }
+
+    @GetMapping("/edit")
+    public String editForm(){
+        return "basic/editForm";
     }
 
     /**
