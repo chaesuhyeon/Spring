0fcd2948f2dca56f02953308fe06619abfadac12
suhyeon <ddd8177@gmail.com>
2022-10-05 13:05:37 +0900

    타임리프 조건식

--- thymeleaf-basic/src/main/java/helllo/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/helllo/thymeleaf/basic/BasicController.java
@@ -93,6 +93,12 @@ public class BasicController {
         return "basic/attribute";
     }
 
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+        return "basic/condition";
+    }
+
     @Data
     static class User{
         private String username;
