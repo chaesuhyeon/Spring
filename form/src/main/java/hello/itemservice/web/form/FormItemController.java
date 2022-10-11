4e2c87f9642c0dfa459b4fc8dfec0407cde5b13a
suhyeon chae <ddd8177@gmail.com>
2022-10-11 22:16:48 +0900

    타임리프 select box

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.ItemType;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,6 +39,16 @@ public class FormItemController {
 //        ItemType[] values = ItemType.values(); ->  ItemType.values면 ENUM에 있는 값들을 배열로 반환해줌 [BOOK,FOOD,ETC]
 //        return values;
         return ItemType.values(); // 위 코드를 inline으로 리펙토링
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes(){
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST" , "빠른배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL" , "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW" , "느린 배송"));
+        return deliveryCodes;
+        
     }
 
     @GetMapping
