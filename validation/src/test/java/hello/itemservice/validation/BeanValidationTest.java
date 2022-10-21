1bf77177a91c8af086f1e9e2ccf97a3d557c93a8
chaesuhyeon <ddd8177@gmail.com>
2022-10-21 23:03:17 +0900

    Bean Validation 스프링 적용

---
+++ validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,38 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @Test
+    void beanValidation(){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" "); // 공백
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item); // 빈값이면 오류가 있는거고 빈값이 아니면 오류가 있는 것
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation = " + violation.getMessage());
+        }
+    }
+}
+
+//violation = ConstraintViolationImpl{interpolatedMessage='9999 이하여야 합니다', propertyPath=quantity, rootBeanClass=class hello.itemservice.domain.item.Item, messageTemplate='{javax.validation.constraints.Max.message}'}
+//violation = 9999 이하여야 합니다
+//violation = ConstraintViolationImpl{interpolatedMessage='공백일 수 없습니다', propertyPath=itemName, rootBeanClass=class hello.itemservice.domain.item.Item, messageTemplate='{javax.validation.constraints.NotBlank.message}'}
+//violation = 공백일 수 없습니다
+//violation = ConstraintViolationImpl{interpolatedMessage='1000에서 1000000 사이여야 합니다', propertyPath=price, rootBeanClass=class hello.itemservice.domain.item.Item, messageTemplate='{org.hibernate.validator.constraints.Range.message}'}
+//violation = 1000에서 1000000 사이여야 합니다
