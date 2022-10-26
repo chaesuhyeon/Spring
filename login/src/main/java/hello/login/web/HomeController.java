a9267e064e3c274d50bc605dfa075bea495f2009
suhyeon chae <ddd8177@gmail.com>
2022-10-26 20:08:45 +0900

    로그인 - 쿠키 사용(보안 문제 존재)

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,44 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
+//    public String home() {
+//        return "home";
+//    }
+
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String homeLogin(@CookieValue(name = "memberId" , required = false) Long memberId , Model model){
+        // CookieValue -> 저장된 쿠키 받아옴
+        // required = false -> 로그인 하지 않은 사용자(쿠키값이 없는 사용자)도 들어와야하기 때문
+
+        // 로그인 안한 경우
+        if(memberId == null){
+            return "home";
+        }
+
+        // 로그인 성공
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
     }
 }
--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -25,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form , BindingResult bindingResult){
+    public String login(@Valid @ModelAttribute LoginForm form , BindingResult bindingResult , HttpServletResponse response){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
         }
@@ -38,6 +40,22 @@ public class LoginController {
         }
 
         // 로그인 성공 처리 TODO
+        //쿠키에 시간 정보를 주지 않으면 세션쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId())); // 로그인에 성공하면 쿠키 생성
+        response.addCookie(idCookie); // 생성한 쿠키를 HttpServletResponse에 담는다. 쿠키 이름은 memberId이고, 이 값은 회원의 id를 담아둔다. 웹브라우저는 종료 전까지 회원의 id를 서버에 계속 보내줄 것이다.
+
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response , "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response , String cookieName) {
+        Cookie cookie = new Cookie(cookieName , null);
+        cookie.setMaxAge(0); // 쿠키 시간을 0으로 하면 삭제
+        response.addCookie(cookie);
     }
 }
