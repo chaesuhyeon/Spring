44ca5ff7ae04a51d33a4c6a3fa10164c60110a01
chaesuhyeon <ddd8177@gmail.com>
2022-11-05 23:02:13 +0900

    로그인 처리하기 - 서블릿 HTTP 세션

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -46,7 +48,7 @@ public class HomeController {
 
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model){
 
         //세션 관리자에 저장된 회원 정보 조회
@@ -61,5 +63,41 @@ public class HomeController {
         model.addAttribute("member", member);
         return "loginHome";
 
+    }
+
+//    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model){
+
+        HttpSession session = request.getSession(false);
+
+        // 로그인
+        if (session == null){ // 세션이 없으면 home으로 보냄
+            return "home";
+        }
+
+        //세션에 저장된 회원 정보 조회
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null){
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
+
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null){
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
