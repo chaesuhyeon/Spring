e9f81f8ad70f6beba31eaabd2ebf910284fb1896
chaesuhyeon <ddd8177@gmail.com>
2022-12-09 23:20:13 +0900

    영속성 관리 - 내부 동작 방식

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -10,6 +10,14 @@ public class Member {
     private Long id;
     private String name;
 
+    public Member(){ // 기본 생성자 필수
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
