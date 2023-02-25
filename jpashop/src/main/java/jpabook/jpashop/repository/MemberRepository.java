2f84a62a3df224f1166615b8e0703f369c1087e7
suhyeon chae <ddd8177@gmail.com>
2023-02-25 23:04:42 +0900

    회원 도메인 기능 개발 완료

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,11 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository // @Repository를 사용하면 컴포넌트 스캔해서 자동으로 스프링 빈으로 관리해주게 함
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em; // 스프링이 엔티티 매니저를 만들어서 자동으로 주입시켜줌
+//    @PersistenceContext -> 이 어노테이션 사용해도 되는데,  spring data jpa가 autowired로 사용하도록 지원해줘서 @RequiredArgsConstructor를 사용해도 된다.
+    private final EntityManager em; // 스프링이 엔티티 매니저를 만들어서 자동으로 주입시켜줌
 
     public void save(Member member){
         em.persist(member);
---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional //jpa에서 같은 트랜잭션 안에서 pk값이 똑같으면 같은 영속성 컨텍스트에서 똑같은 애로 관리가 됨
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+//    @Rollback(value = false) // @Transactional가 test에 있으면 rollback 해주기 때문에 rollback 을 안하고 싶다면 @Rollback(value=false)해주면 된다.
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("Kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        Assertions.assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+
+        //memberService.join(member2); // 예외가 발생해야 한다!! / IllegalStateException: 이미 존재하는 회원입니다.
+
+        //then
+
+        //Junit5 이용할 때 exception 발생하는 경우 test
+        IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertEquals("이미 존재하는 회원입니다.", thrown.getMessage());
+
+        Assertions.fail("예외가 발생해야 한다."); // 이미 위에서 Exception이 발생해서 여기까지 오면 안된다. 여기까지 와도 test 실패
+    }
+
+}
