7bc18c85a738aa1162eaeb51b4e8a68a69642ada
suhyeon <ddd8177@gmail.com>
2023-02-28 17:31:39 +0900

    상품 등록 / 상품 목록 화면 기능 완료

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain.item;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@Getter @Setter
 @DiscriminatorValue("B") // single table(상속관계) 전략이므로 데이터를 저장할때 어떤 테이블인지 구분할 수 있어야 하므로 @DiscriminatorValue 사용
 public class Book extends Item{ // Item 상속 받음
 
