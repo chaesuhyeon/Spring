300fb7f4b0991a35230fe259ae4f1024f6719100
suhyeon <ddd8177@gmail.com>
2023-02-28 16:58:33 +0900

    회원 등록 / 목록 화면 기능 완료

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.web.MemberForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form , BindingResult result){
+
+        if(result.hasErrors()){ // MemberForm 객체를 validation 했을 때 통과 못했으면(error 발생) 회원 등록 폼으로 보낸다.
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode()); // form에 있는 것을 꺼내서 Address를 생성
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+}
