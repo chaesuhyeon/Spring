58e9c2b745ccc97a33fcdb515b88e3d274436f51
chaesuhyeon <ddd8177@gmail.com>
2023-03-05 19:25:09 +0900

    간단한 주문 조회 - 패치 조인 최적화

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -54,6 +54,19 @@ public class OrderSimpleApiController {
         return result;
     }
 
+    /**
+     * 패치조인 사용
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
 
 
     @Data
