c4f71dd67758f008083498c8da5a0869e83e281b
suhyeon <ddd8177@gmail.com>
2023-03-13 12:57:42 +0900

    jpa save 동작 확인

--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,13 +1,17 @@
 package study.datajpa.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class MemberJpaRepositoryTest {
 
     @Autowired
@@ -17,10 +21,14 @@ class MemberJpaRepositoryTest {
     public void testMember() throws Exception {
         //given
         Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
 
         //when
+        Member findMember = memberJpaRepository.find(savedMember.getId());
 
         //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
     }
 
 }
