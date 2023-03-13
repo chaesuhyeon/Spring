bc4197d1383d4dfaf0b18c222585e1ae6d57ec87
suhyeon chae <ddd8177@gmail.com>
2023-03-13 23:19:25 +0900

    예제 도메인 모델 설계

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,23 +1,36 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 만들어줘야함
+@ToString(of = {"id", "username", "age"}) // 객체 바로 찍어도 출력되게 , 연관관계 필드 (ex.team)은 toString에 넣게 되면 서로 참조하게 돼서 무한루프 돌 수 있으니 넣지 말아야함
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
-    protected Member(){
-    }
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id") // team_id 는 fk , member가 연관관계의 주인 (외래키를 가지고 있기 때문에)
+    private Team team;
+
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+
+        if(team != null){
+            changeTeam(team);
+        }
     }
 
     /**
@@ -25,6 +38,12 @@ public class Member {
      */
     public void changeUsername(String username){
         this.username = username;
+    }
+
+    //==연관관계 메서드==//
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,30 +1,30 @@
 package study.datajpa.repository;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class MemberJpaRepositoryTest {
-
+@Rollback(value = false)
+class MemberRepositoryTest {
     @Autowired
-    MemberJpaRepository memberJpaRepository;
+    MemberRepository memberRepository;
 
     @Test
-    public void testMember() throws Exception {
+    public void testMember(){
         //given
         Member member = new Member("memberA");
-        Member savedMember = memberJpaRepository.save(member);
+        Member savedMember = memberRepository.save(member);
 
         //when
-        Member findMember = memberJpaRepository.find(savedMember.getId());
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
 
         //then
         assertThat(findMember.getId()).isEqualTo(member.getId());
