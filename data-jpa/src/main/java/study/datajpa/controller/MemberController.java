da3b45b39e8036be987ba8f650d2bf70a5ea0654
suhyeon chae <ddd8177@gmail.com>
2023-03-20 22:04:12 +0900

    feat: 페이징과 정렬(Web) & 새로운 엔티티를 구별하는 방법

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -31,9 +35,20 @@ public class MemberController {
         return member.getUsername();
     }
 
+    /**
+     * 페이징과 정렬
+     */
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable){
+        return memberRepository.findAll(pageable)// findAll에 pageable을 넘길 수 있다.
+                            .map(MemberDto::new);
+    }
+
 
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user"+i,i));
+        }
     }
 }
