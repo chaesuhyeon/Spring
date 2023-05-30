8b77b2351c32af9562b9d305d303b67cec3ece1e
chaesuhyeon <ddd8177@gmail.com>
2023-05-30 10:12:00 +0900

    예제로 구현하는 파일 업로드 다운로드

--- upload/src/main/java/hello/upload/controller/ItemController.java
+++ upload/src/main/java/hello/upload/controller/ItemController.java
@@ -6,16 +6,20 @@ import hello.upload.domain.UploadFile;
 import hello.upload.file.FileStore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -63,5 +67,39 @@ public class ItemController {
         model.addAttribute("item", item);
 
         return "item-view";
+    }
+
+    /**
+     * 이미지 가져오기
+     * 경로에 있는 파일에 접근해서 파일을 스트림으로 반환
+     */
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        return new UrlResource("file:" + fileStore.getFullPath(filename));
+    }
+
+    /**
+     * 파일 다운로드
+     */
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName)); // 서버에 저장된 파일 이름이 있는 fullPath
+
+        log.info("uploadFileName : {}" , uploadFileName);
+
+        // 한글 깨질 수도 있으니 인코딩 해주기
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+
+        // 다운로드 하려면 헤더에 contentDisposition 추가해야함
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition) // 이 부분 추가하지 않으면 파일이 다운로드 되지 않고 파일의 내용이 보임
+                .body(resource);
     }
 }
