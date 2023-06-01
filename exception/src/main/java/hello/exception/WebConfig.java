d9a74a8497c010397fe19be4cb9fd91594d76389
chaesuhyeon <ddd8177@gmail.com>
2023-06-01 22:24:51 +0900

    서블릿 예외처리 - 인터셉터

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -7,10 +7,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import hello.exception.interceptor.LogInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/css/**", "/*.ico"
+                        , "/error", "/error-page/**" //오류 페이지 경로
+                );
+    }
+
+    //@Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new
                 FilterRegistrationBean<>();
