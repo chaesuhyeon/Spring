bee046f849a6c458233b2824d6b4bb6db98042bd
suhyeon chae <ddd8177@gmail.com>
2023-06-13 19:35:30 +0900

    로그 추적기 V2 적용

---
+++ advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace; // @component이기 때문에 자동으로 스프링 빈으로 등록
+
+    public void orderItem(TraceId traceId, String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status,e);
+            throw  e;
+            /*예외를 꼭 다시 던져주어야 한다. 그렇지 않으면 여기서 예외를 먹어버리고, 이후에 정상
+            흐름으로 동작한다. 로그는 애플리케이션에 흐름에 영향을 주면 안된다. 로그 때문에 예외가 사라지면
+            안된다.*/
+        }
+    }
+}
