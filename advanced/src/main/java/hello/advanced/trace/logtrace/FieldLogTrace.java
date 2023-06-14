77c130dc9057dccca9f1bccb8ecd7b942057d339
suhyeon chae <ddd8177@gmail.com>
2023-06-14 21:57:07 +0900

    로그 추적기 > 필드 동기화 - 개발

---
+++ advanced/src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
@@ -0,0 +1,79 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FieldLogTrace implements LogTrace{
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+    
+    private TraceId traceIdHolder; // tracdId 동기화 , 동시성 이슈 발생
+
+    @Override
+    public TraceStatus begin(String message) {
+        syncTraceId();
+        TraceId traceId = traceIdHolder; // sync하고 나면 traceHolder에 값이 들어간것 보장됨
+        
+        long startTimeMs = System.currentTimeMillis();
+
+        // 로그 출력
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX,
+                traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    @Override
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void syncTraceId() {
+        if(traceIdHolder == null) { // null 이면 traceId를 새로 만듦 (처음의 경우)
+            traceIdHolder = new TraceId();
+        } else { // 처음이 아닌 경우
+            traceIdHolder = traceIdHolder.createNextId();
+        }
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(),
+                    addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(),
+                    resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),
+                    addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs,
+                    e.toString());
+        }
+
+        releaseTraceId();
+    }
+
+    private void releaseTraceId() {
+        if(traceIdHolder.isFirstLevel()) { // 레벨이 첫번째는 마지막에 왔다는 뜻
+            traceIdHolder = null; // destroy // 없애줌
+        } else { // 마지막이 아니라면 이전꺼 호출
+            traceIdHolder = traceIdHolder.createPreviousId();
+        }
+    }
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append( (i == level - 1) ? "|" + prefix : "| ");
+        }
+        return sb.toString();
+    }
+}
