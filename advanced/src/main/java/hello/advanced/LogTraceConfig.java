5aa20b90de0bc1dba2bbed1e7212ccdd74b8e120
chaesuhyeon <ddd8177@gmail.com>
2023-06-23 12:42:59 +0900

    ThreadLocal 로컬 - 적용

--- advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -2,6 +2,7 @@ package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,6 @@ import org.springframework.context.annotation.Configuration;
 public class LogTraceConfig {
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace(); // 싱글톤으로 빈이 등록된다 (중요)
+        return new ThreadLocalLogTrace(); // 싱글톤으로 빈이 등록된다 (중요)
     }
 }
