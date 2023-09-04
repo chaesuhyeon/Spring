8368453b8c18be56053d70b46afd586494be8f54
chaesuhyeon <ddd8177@gmail.com>
2023-09-05 08:08:27 +0900

    컨버젼 서비스에 포맷터 등록 및 웹 애플리케이션에 포맷터 적용

--- typeconverter/src/main/java/helllo/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/helllo/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import helllo.typeconverter.converter.IntegerToStringConverter;
 import helllo.typeconverter.converter.IpPortToStringConverter;
 import helllo.typeconverter.converter.StringToIntegerConverter;
 import helllo.typeconverter.converter.StringToIpPortConverter;
+import helllo.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,9 +19,16 @@ public class WebConfig implements WebMvcConfigurer {
     /* addFormatters : converter의 확장된 버전 */
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+
+        // 우선순위 때문에 주석처리한다. (MyNumberFormatter도 숫자 <-> 문자 이기 때문에 포맷터를 동작시키기 위해 주석처리 한다.)
+/*        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());*/
+
+        // 컨버터 등록
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // 포맷터 등록
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
