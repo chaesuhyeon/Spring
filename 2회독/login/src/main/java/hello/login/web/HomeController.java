47f53c3d84ee3cd6d5e2b005148e3a9658dcb426
suhyeon <ddd8177@gmail.com>
2023-09-11 07:35:02 +0900

    직접 만든 session 적용

--- 2회독/login/src/main/java/hello/login/web/HomeController.java
+++ 2회독/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,12 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 //    @GetMapping("/")
     public String home() {
@@ -25,7 +29,7 @@ public class HomeController {
      * 로그인 안한 사용자도 home을 들어올 수 있어야 하기 때문에 required = false
      * cookie의 값은 String으로 저장했는데 Long으로 받을 수 있다. -> typeconverter가 자동으로 변환해준다.
      */
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if (memberId == null) {
             return "home";
@@ -39,6 +43,22 @@ public class HomeController {
 
         // cookie에도 값이 있고 db에도 값이 있으면 model에 담아준다.
         model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    /* 세션 사용 */
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        // 로그인
+        if(member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
         return "loginHome";
     }
 }
