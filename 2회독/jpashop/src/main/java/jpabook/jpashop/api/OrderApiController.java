b373fcfd7486110524490b117244546422e1860a
suhyeon <ddd8177@gmail.com>
2023-09-26 12:18:27 +0900

    주문 조회(xTOMany) V3.1(batch size), V4 DTO 직접 조회(최적화x)

--- 2회독/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ 2회독/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,9 +6,12 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -19,6 +22,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     /**
      * 엔티티 그대로 반환
@@ -61,6 +65,29 @@ public class OrderApiController {
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
     }
+
+    /**
+     * 페이징 가능
+     */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        return orders.stream()
+                .map(OrderDto::new) // .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     *  N + 1문제
+     */
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
 
     @Getter
     static class OrderDto {
