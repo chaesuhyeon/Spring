1e7703053e73900b10447c36cad959785bd1d3d5
suhyeon chae <ddd8177@gmail.com>
2023-12-31 14:40:13 +0900

    스프링 프레임 워크로 변환

--- 2회독/core/src/main/java/hello/core/AppConfig.java
+++ 2회독/core/src/main/java/hello/core/AppConfig.java
@@ -1,31 +1,41 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
  * 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다.
  */
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private static FixDiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public static DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
